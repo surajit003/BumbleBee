19de978272b5abc61a835412b5f916905abb2fcc
surajit <surajit@poweredbypeople.io>
2020-11-01 14:21:11 +0300

    added decorator pattern for patting handler with routes

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-from webob import Request, Response
+
+from api import API
+
+app = API()
+
+@app.route('/home')
+def home(request, response):
+    response.text = "Hello from the HOME page"
+
+@app.route('/about')
+def about(request, response):
+    response.text = "Hello from the ABOUT page"
